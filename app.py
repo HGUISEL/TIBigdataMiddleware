41be400a4896b03fecee8cab9d38cf5832d910f0
Sapphire317 <sdw9090@naver.com>
2019-10-23 17:52:14 +0900

    v1.0.0:

--- app.py
+++ app.py
@@ -34,12 +34,12 @@ from krwordrank.word import KRWordRank
 #Url address of Elasticsearch
 
 serverUrl="http://203.252.103.86:8080"
-localUrl="http://localhost:9200"
+# localUrl="http://localhost:9200"
 
 
 
 #ElasticSearch connection
-es = Elasticsearch(serverUrl)
+es=Elasticsearch(serverUrl)
 app = Flask(__name__)
 api = Api(app)
 
@@ -48,15 +48,10 @@ api = Api(app)
 CORS(app, support_credentials=True)
 
 
-#text - the text to summarize
-#ratio (float, optional) - The ratio of sentence of original text
-#word_count (int, optional) - the number of word to print. if ratio and word_count aregiven, ratio will be ignored.
-#split (bool, optional) - if it is true, the list of sentence will be returned.
-@app.route('/summ', methods=['GET'])
-def summ():
-    results = es.get(index='text', doc_type='text', id='1')
-    contents = json.dumps(results['_source']["attachment"]["content"], ensure_ascii=False)
 
+@app.route("/hello")
+def hello():
+    contents = json.dumps("한글")
     return contents
 
 
@@ -65,162 +60,176 @@ def summ():
 def wordRank():
 
     #Retreive text from elasticsearch
-    results = es.get(index='text', doc_type='text', id='1')
-    texts = json.dumps(results['_source']["attachment"]["content"], ensure_ascii=False)
-
-    #split the text by sentences
-    sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', texts)
+    results = es.get(index='crawling', doc_type='nkdboard', id='5d76f149a2e5d0edebd8522f')
+    texts = json.dumps(results['_source'], ensure_ascii=False)
+    
+    # #split the text by sentences
+    # sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', texts)
     
 
-    #normalize the text
-    texts = [normalize(text, number=True) for text in sentences]
+    # #normalize the text
+    # texts = [normalize(text, number=True) for text in sentences]
     
-    wordrank_extractor = KRWordRank(
-        min_count = 7,  # Minimum frequency of word
-        max_length=10,  # Maximum length of word
-        verbose = True
-    )
+    # wordrank_extractor = KRWordRank(
+    #     min_count = 7,  # Minimum frequency of word
+    #     max_length=10,  # Maximum length of word
+    #     verbose = True
+    # )
 
-    beta = 0.85  # Decaying factor beta of PageRank
-    max_iter = 10
+    # beta = 0.85  # Decaying factor beta of PageRank
+    # max_iter = 10
   
-    keywords, rank, graph = wordrank_extractor.extract(texts, beta, max_iter)
+    # keywords, rank, graph = wordrank_extractor.extract(texts, beta, max_iter)
     
-    result=[]
-    dic={}
-    #Make a dictionary [word, weight]
-    for word, r in sorted(keywords.items(), key=lambda x:x[1], reverse=True)[:30]:
-        dic["y"]=r
-        dic["label"]=word
-        result.append(dic)
-        dic={}
+    # result=[]
+    # dic={}
+    # #Make a dictionary [word, weight]
+    # for word, r in sorted(keywords.items(), key=lambda x:x[1], reverse=True)[:30]:
+    #     dic["y"]=r
+    #     dic["label"]=word
+    #     result.append(dic)
+    #     dic={}
 
-    return json.dumps(result, ensure_ascii=False)
+    return json.dumps(results, ensure_ascii=False)
 
 
 
-# @app.route('/ext', methods=['GET'])
-# def extractEntity():
+# # @app.route('/ext', methods=['GET'])
+# # def extractEntity():
     
     
-#     # ETRI API KEY
-#     API_KEY = '2aca8d0f-ccdb-4074-ba54-de2b3c2a7bec'
-#      #Retreive text from elasticsearch
+# #     # ETRI API KEY
+# #     API_KEY = '2aca8d0f-ccdb-4074-ba54-de2b3c2a7bec'
+# #      #Retreive text from elasticsearch
+# #     results = es.get(index='text', doc_type='text', id='1')
+# #     texts = json.dumps(results['_source']["attachment"]["content"], ensure_ascii=False)
+
+# #     recognizer = EntityRecognizer(API.ETRI, API_KEY)
+
+# #     parsed = recognizer.analyze("안녕하세요 문재인입니다. 하늘에 계신 우리아버지여.")
+    
+# #     print(parsed)
+# #     for entity in parsed[0].getEntities():
+# #         print(entity)
+
+
+
+
+# #     return "Good"
+
+
+# @app.route('/term')
+# def term():
+#     finalize()
+#     return "Terminated"
+
+
+# @app.route('/', methods=['GET'])
+# def index():
 #     results = es.get(index='text', doc_type='text', id='1')
-#     texts = json.dumps(results['_source']["attachment"]["content"], ensure_ascii=False)
-
-#     recognizer = EntityRecognizer(API.ETRI, API_KEY)
-
-#     parsed = recognizer.analyze("안녕하세요 문재인입니다. 하늘에 계신 우리아버지여.")
-    
-#     print(parsed)
-#     for entity in parsed[0].getEntities():
-#         print(entity)
-
-
-
-
-#     return "Good"
-
-
-@app.route('/term')
-def term():
-    finalize()
-    return "Terminated"
-
-
-@app.route('/', methods=['GET'])
-def index():
-    results = es.get(index='text', doc_type='text', id='1')
    
-    return json.dumps(results['_source'], ensure_ascii=False)
+#     return json.dumps(results['_source'], ensure_ascii=False)
 
-@app.route('/search', methods=['POST', 'GET'])
-def search():
+# @app.route('/search', methods=['POST', 'GET'])
+# def search():
     
-    keyword=request.json["keyword"]
-    body= {
-        "query" : {
-            "match_all":{
+#     keyword=request.json["keyword"]
+#     body= {
+#         "query" : {
+#             "match_all":{
                 
-            }
-        }
-    }
+#             }
+#         }
+#     }
 
-    res = es.search(index="text", body=body)
+#     res = es.search(index="text", body=body)
 
-    return json.dumps(res["hits"]["hits"], ensure_ascii=False)
+#     return json.dumps(res["hits"]["hits"], ensure_ascii=False)
+
 
 
 @app.route('/test', methods=['POST', 'GET'])
 def test():
     if request.method=='POST':
        result=request.json 
-
        keyword=result["keyword"]
 
-       
-       body={
-           "query" : {
-               "match_phrase_prefix" : {
-                   "bodys":keyword
-               }
-           }
-       }
 
-       es_results = es.search(index='crawlingdata', body=body)
-    resultSet = es_results["hits"]["hits"]
+    body = {
+        "query" : {
+            "match_all" : {}
+        },
+        "size" : 1000,
+    }
+    
+    res= es.search(index="crawling", body=body)
 
-    final = []
+    resultArr = res["hits"]["hits"]
 
-    for i in resultSet:
-        body=i["_source"]["bodys"]
-        title=i["_source"]["titles"][0]
-        date=i["_source"]["dates"][0]
-        url=i["_source"]["urls"]
-        writer=i["_source"]["writers"][0]
+    dateArr=[]
 
-        wordSet = rank(body)
-        final.append(wordSet)
+    print(resultArr)
+
+    dayCntDic={}
+
+
+    for i in resultArr:
+        dateArr.append(i["_source"]["dates"])
 
     
-    print(final[0])
-              
-    #    print(result["keyword"])
-   
-    return json.dumps(es_results["hits"]["hits"], ensure_ascii=False)
+    for i in dateArr:
+        day=i[8:10]
+        
+        if day in dayCntDic:
+            dayCntDic[day] +=1
+        else:
+            dayCntDic[day] =1
 
 
-
-def rank(contents):
-     #split the text by sentences
-    sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', contents)
-    
-
-    #normalize the text
-    contents = [normalize(text, number=True) for text in sentences]
-    
-    wordrank_extractor = KRWordRank(
-        min_count = 7,  # Minimum frequency of word
-        max_length=10,  # Maximum length of word
-        verbose = True
-    )
-
-    beta = 0.85  # Decaying factor beta of PageRank
-    max_iter = 10
-  
-    keywords, rank, graph = wordrank_extractor.extract(contents, beta, max_iter)
-    
-    result=[]
+    resultDic=[]
     dic={}
-    #Make a dictionary [word, weight]
-    for word, r in sorted(keywords.items(), key=lambda x:x[1], reverse=True)[:30]:
-        dic["y"]=r
-        dic["label"]=word
-        result.append(dic)
+
+    for day, cnt in sorted(dayCntDic.items()):
+        dic["y"]=cnt
+        dic["label"]=day
+        resultDic.append(dic)
         dic={}
+
+
+    return json.dumps(resultDic, ensure_ascii=False)
+ 
+
+
+
+# def rank(contents):
+#      #split the text by sentences
+#     sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', contents)
     
-    return result
+
+#     #normalize the text
+#     contents = [normalize(text, number=True) for text in sentences]
+    
+#     wordrank_extractor = KRWordRank(
+#         min_count = 7,  # Minimum frequency of word
+#         max_length=10,  # Maximum length of word
+#         verbose = True
+#     )
+
+#     beta = 0.85  # Decaying factor beta of PageRank
+#     max_iter = 10
+  
+#     keywords, rank, graph = wordrank_extractor.extract(contents, beta, max_iter)
+    
+#     result=[]
+#     dic={}
+#     #Make a dictionary [word, weight]
+#     for word, r in sorted(keywords.items(), key=lambda x:x[1], reverse=True)[:30]:
+#         dic["y"]=r
+#         dic["label"]=word
+#         result.append(dic)
+#         dic={}
+    
+#     return result
 
 @app.after_request
 def after_request(response):
