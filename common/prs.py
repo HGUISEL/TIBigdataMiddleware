c6ef38dc0e9a73d16749ae547ac2f03acf0547b5
BaekTree <etknbe@gmail.com>
2020-01-08 11:59:05 +0900

    refactoring

--- common/prs.py
+++ common/prs.py
@@ -1,10 +1,17 @@
 # KUBIC module : prs.py
-# created Data : 오늘 :)
+# created Data : 어제 :(
 # purpose : 데이터 전처리 모듈
 import traceback
 # from datetime import datetime
+import os
+import sys
+
+file_dir = os.path.dirname(__file__)
+sys.path.append(file_dir)
+from cmm import showTime
+from cmm import DocCorpus as dc
 import esFunc
-import time
+# import time
 from konlpy.tag import Okt
 
 #RANDOM_MODE
@@ -16,25 +23,14 @@ RANDOM_MODE = False
 # without HGU-WLAN network, use raw data sample no matter this value
 BACKEND_CONCT = True
 
- # global variables
-NUM_DOC = 5
-titles = []
-contents = []
-start = None
-# ES_INDEX = 'nkdboard'
-# ES_INDEX = 'kolofoboard'
+
 
 # Sample Raw Data from Backend directory
 DIR_SMP_DATA = "./raw data sample/rawData.json"
 
-# time taken evaluation
-def showTime():
-    global start
-    seconds = time.time() - start
-    m, s = divmod(seconds, 60)
-    h, m = divmod(m, 60)
-    # print("투입된 문서의 수 : %d\n설정된 Iteratin 수 : %d\n설전된 토픽의 수 : %d" %(NUM_DOC, NUM_ITER, NUM_TOPICS))
-    print("%d 시간 : %02d 분 : %02d 초 " % (h, m, s))
+
+# print("in prs.py gloabl, dc.NUM_DOC : ", dc.NUM_DOC)
+
 
 # Phase 1 : ES에서 문서 쿼리 및 content와 title 분리 전처리
 def loadData():
@@ -42,12 +38,13 @@ def loadData():
     import json
     import sys
     import traceback
-    global NUM_DOC
+    # print(dc.N
+    # UM_DOC)
     print("데이터 로드 중...")
     try :
         if BACKEND_CONCT == False:
             raise Exception("서버 연결 불가")
-        corpus = esFunc.esGetDocs(NUM_DOC)
+        corpus = esFunc.esGetDocs(dc.NUM_DOC)
         print("connection to Backend server succeed!")
         print(len(corpus),"개의 문서를 가져옴")# 문서의 수... 내용 없으면 뺀다...
 
@@ -62,7 +59,7 @@ def loadData():
         
         print("connection to Backend server failed!")
     showTime() 
-    NUM_DOC = len(corpus) # 전체 사용 가능한 문서 수를 업데이트한다. 
+    dc.NUM_DOC = len(corpus) # 전체 사용 가능한 문서 수를 업데이트한다. 
     print("문서 로드 완료!")
     print()
 
@@ -76,19 +73,20 @@ def loadData():
     for idx, doc in enumerate(corpus):
         # print(doc["content"])
         if doc["content"] != "":
-            titles.append(doc["post_title"])
-            contents.append(doc["content"])
+            dc.titles.append(doc["post_title"])
+            dc.contents.append(doc["content"])
         else:
             count += 1
 
-    num_doc = len(contents)
+    dc.NUM_DOC = len(dc.contents)
     print(count,"개의 문서가 내용이 없음")
-    # print(titles)#순서가 뒤바뀐 문서 set을 출력
-    print("투입된 문서의 수 : %d" %(num_doc))
-    # print(len(contents))
+    # print(dc.titles)#순서가 뒤바뀐 문서 set을 출력
+    print("투입된 문서의 수 : %d" %(dc.NUM_DOC))
+    # print(len(dc.contents))
 
-    # update NUM_DOC
-    return num_doc
+    # update dc.NUM_DOC
+    # return num_doc
+    return
 
 # phase 2 형태소 분석기 + 내용 없는 문서 지우기
 def dataPrePrcs():
@@ -96,10 +94,10 @@ def dataPrePrcs():
     # 형태소 분석기 instance
     okt = Okt()
     print("데이터 전처리 중... It may takes few hours...")
-    tokenized_doc = [okt.nouns(contents[cnt]) for cnt in range(len(contents))]
+    tokenized_doc = [okt.nouns(dc.contents[cnt]) for cnt in range(len(dc.contents))]
 
     print("형태소 분석 완료!")
-    print("투입된 문서의 수 : %d" %(NUM_DOC))
+    print("투입된 문서의 수 : %d" %(dc.NUM_DOC))
     showTime()
 
     # 한글자 단어들 지우기!
@@ -121,22 +119,17 @@ def dataPrePrcs():
                 ...
            ]
 """
-def readyData(num_doc = NUM_DOC):
-
-    # time taken evaluation
-    global start
-    start = time.time()
-
-    # NUM_DOC initialize
-    global NUM_DOC
-    if num_doc != NUM_DOC:
-        NUM_DOC = num_doc
+def readyData(num_doc):
+    # dc.NUM_DOC initialize
+    dc.NUM_DOC = num_doc
+    
+    # print("in readyData after if, ", dc.NUM_DOC)
 
     # Phase 1 : ES에서 문서 쿼리 및 content와 title 분리 전처리
     print("\n\n#####Phase 1-1 : 데이터 로드 실행#####")
-    NUM_DOC = loadData()# load data and update NUM_DOC
+    loadData()# load data and update dc.NUM_DOC
 
     # phase 2 형태소 분석기 + 내용 없는 문서 지우기
     print("\n\n#####Phase 1-2 : 데이터 전처리 실행#####")
     tokenized_doc = dataPrePrcs()
-    return tokenized_doc
+    return dc.titles, tokenized_doc
