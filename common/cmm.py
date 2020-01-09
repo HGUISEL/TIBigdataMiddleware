00cc9e126146d2e1f3dbb5a98eb98a1bacae38d7
BaekTree <etknbe@gmail.com>
2020-01-09 22:21:18 +0900

    del glob att, path handling, glob dir set

--- common/cmm.py
+++ common/cmm.py
@@ -8,14 +8,49 @@ import time
 from datetime import datetime
 
 # Sample Raw Data from Backend directory
-SAMP_DATA_DIR = '../raw data sample/'
+# if
+# currDir = os.getcwd()
+# import os
+# currDirPath = os.getcwd()
+# currDir = os.path.split(currDirPath)[1]
+# if currDir != common:
+#     os.chdir()
+# print("directory!")
+# print(os.path.dirname(currDir))
+# from pathlib import Path
+# print(Path(currDir).parent)
+# os.chdir(r'C:\Folder')
+# currDir = os.chdir
+# print( type(currDir ))
+# if()
+
+# print("\n#########directory path adjustment process##########")
+import os
+currDirPath = os.getcwd()
+currDir = os.path.split(currDirPath)[1]
+# parentDirPath = os.path.split(currDirPath)[0]
+# parDir = os.path.split(parentDirPath)[1]
+if currDir != "common":
+    # try:
+    if currDir == "TIBigdataMiddleware":
+        os.chdir(currDirPath+"\\common")
+        print("dir path adjusted!")
+    else:
+        print("dir path error! check file cmm.py")
+# else:
+    # print("nothing woring with dir parh!")
+    # except Exception as e:
+# print("#####dir path procs fin!#####\n")
+
+SAMP_DATA_DIR = "../raw data sample/rawData.json" 
+# SAMP_DATA_DIR = '../raw data sample/'
 
 # global variables
-class DocCorpus:
-    NUM_DOC = 0
-    titles = []
-    contents = []
-    idList = []    
+# class DocCorpus:
+#     NUM_DOC = 0
+#     titles = []
+#     contents = []
+#     idList = []    
     # def __init__ (self):
     #     self.NUM_DOC = 5
     #     self.titles = []
@@ -25,6 +60,7 @@ class DocCorpus:
 
 # current date and time
 now = datetime.now()
+print("now is the time : ", now)
 start = time.time()
 # print("start time : ", now)# 이 모듈을 import할 때 바로 global scope 실행.
 
