2227bfbc38669a78ef1d529060ca26c6eb20d06a
BaekTree <etknbe@gmail.com>
2020-01-12 20:56:13 +0900

    clean comments

--- common/cmm.py
+++ common/cmm.py
@@ -7,63 +7,17 @@ purpose : common features for all algorithm usage
 import time
 from datetime import datetime
 
-# Sample Raw Data from Backend directory
-# if
-# currDir = os.getcwd()
-# import os
-# currDirPath = os.getcwd()
-# currDir = os.path.split(currDirPath)[1]
-# if currDir != common:
-#     os.chdir()
-# print("directory!")
-# print(os.path.dirname(currDir))
-# from pathlib import Path
-# print(Path(currDir).parent)
-# os.chdir(r'C:\Folder')
-# currDir = os.chdir
-# print( type(currDir ))
-# if()
-
-# import os
-# currDirPath = os.getcwd()
-# currDir = os.path.split(currDirPath)[1]
-# if currDir != "common":
-#     # try:
-#     if currDir == "TIBigdataMiddleware":
-#         os.chdir(currDirPath+"\\common")
-#         print("dir path adjusted!")
-#     else:
-#         print("dir path error! check file cmm.py")
-
 SAMP_DATA_DIR = "../raw data sample/rawData30.json" 
 
 LDA_DIR_FE = "../../TIBigdataFE/src/assets/special_first/data.json"
 
-# SAMP_DATA_DIR = '../raw data sample/'
-
-# global variables
-# class DocCorpus:
-#     NUM_DOC = 0
-#     titles = []
-#     contents = []
-#     idList = []    
-    # def __init__ (self):
-    #     self.NUM_DOC = 5
-    #     self.titles = []
-    #     self.contents = []
-# ES_INDEX = 'nkdboard'
-# ES_INDEX = 'kolofoboard'
-
 # current date and time
 now = datetime.now()
-# print("now is the time : ", now)
 print("Warning! : current version has initialize twice the cmm module...need to be fixed later on...")
 start = time.time()
-# print("start time : ", now)# 이 모듈을 import할 때 바로 global scope 실행.
 
 # time taken evaluation
 def showTime():
-    # global start
     seconds = time.time() - start
     m, s = divmod(seconds, 60)
     h, m = divmod(m, 60)
