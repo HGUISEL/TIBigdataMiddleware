9c0ddc518d1d807a48f9c7414039abe7b3e1dd0c
BaekTree <etknbe@gmail.com>
2020-01-23 09:54:33 +0900

    print top 5 list

--- Labs/cosSim/cosSim.py
+++ Labs/cosSim/cosSim.py
@@ -18,6 +18,8 @@ from common import prs
 # print(data)
 
 data = prs.loadData(30)
+# import pandas as pd
+# data = pd.DataFrame.from_dict(data)
 # print(type(data))
 # data = data["contents"]
 # print(data)
@@ -32,4 +34,32 @@ tfidf_mtx = tfidf.fit_transform(data["contents"])
 from sklearn.metrics.pairwise import linear_kernel
 cosine_sim = linear_kernel(tfidf_mtx, tfidf_mtx)
 
-# print(type(cosine_sim[0]))
+# rcmTable = list(enumerate(cosine_sim[3]))
+# print(rcmTable)
+
+# oneDoc = rcmTable[0]
+# sorted(oneDoc[1],)
+# print(sorted(rcmTable, key=operator.itemgetter(1), reverse=True))
+
+# function : 
+def getRcmd(cosine_sim, index):
+    #recommendation table
+    rcmdTbl = list(enumerate(cosine_sim[index]))
+
+    import operator
+    tempSort = sorted(rcmdTbl, key=operator.itemgetter(1), reverse=True)
+    topFiveRcmd = []
+    for i, obj in enumerate(tempSort):
+        if i > 5:
+            break
+        topFiveRcmd.append(obj)
+    
+    return topFiveRcmd
+
+topF = getRcmd(cosine_sim, 5)
+
+for oneDoc in topF:
+    docIdx = oneDoc[0]
+    
+    print(data["titles"][docIdx])
+# print()
