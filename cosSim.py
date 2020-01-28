0edcc9c452390ad91cf200f1c6e20184a1690621
BaekTree <etknbe@gmail.com>
2020-01-28 21:21:23 +0900

    cos Sim comment

--- cosSim.py
+++ cosSim.py
@@ -1,5 +1,5 @@
 ## phase 0. 디렉토리 추가
-## 현재 위치 : \handong\TIBigdataMiddleware\Labs\cosSim>...
+## 현재 위치 : .\TIBigdataMiddleware\Labs\cosSim>...
 ## middleware home에도 dir 추가
 # from pathlib import Path
 # import os
@@ -12,25 +12,27 @@
 # sys.path.append(str(homeDir))
 
 
-
-
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 import operator
-
-# function : 
+"""
+# fnction : getSimTbl(data)
+# purpose : 전달받은 BoW에 대한 코사인 유사도 테이블을 만든다. sk-learn TF-IDF 사용.
+# input : BoW list
+# output : array
+            [
+                [문서 1의 단어 TF-IDF 값들],
+                [문서 2의 단어 TF-IDF 값들],
+                ...
+            ]
+"""
 def getSimTbl(data):
 
-    # ## phase 1: 문서 로드 및 전처리
-    # from common import prs
-
-    # data = prs.loadData(30)
-
-    ## 3: 분석
     tfidf = TfidfVectorizer()
     tfidf_mtx = tfidf.fit_transform(data)
 
     cosine_sim = linear_kernel(tfidf_mtx, tfidf_mtx)
+
     # save tfidf result
     import json
     with open('./cosSim/skl_tfidf.json', 'w') as fp:
@@ -40,11 +42,29 @@ def getSimTbl(data):
     return cosine_sim
 
     
-
+"""
+* **function : getRcmd(idList, calc = False)**
+  * purpose : 전달받은 문서들의 관련문서들을 찍어준다.
+  * input : 문서 id list<string>. 연관 문서를 얻으려는 문서들 list
+  * output : dictionary 
+            {
+                "id" : "abcde", 
+                "rcmd" : [ "A", "B", "C", "D", "E" ], 
+                 "address" : [ "A", "B", "C", "D", "E" ]
+            }
+               
+  * NOTICE : 
+    * calc = True으로 호출하면 새로 서버에서 문서를 로드해서 tf-idf 데이터를 저장하고, 
+                새로운 결과로 프론트엔드에 전달한다.
+    * calc = false으로 호출하면 기존에 저장해둔 정보를 사용한다. 아주 빠르게 프론트엔드에 응답해준다. defualt값.
+"""
 def getRcmd(idList, calc = False):
-    from common import prs
 
+
+    # phase 1: TF-IDF을 새로 업데이트하여 출력할 것인지 결정
     if calc == True:
+    # phase 1-1: 문서 로드 및 전처리
+        from common import prs
         data = prs.loadData(700)
         import json
         with open('./cosSim/data.json', 'w') as fp:
@@ -52,17 +72,17 @@ def getRcmd(idList, calc = False):
 
         cosine_sim = getSimTbl(data["contents"])
     else:
+    # phase 1-2: 저장되어 있는 tf-idf 값과 data 정보 불러옴
         import json
         with open('./cosSim/skl_tfidf.json', 'r') as fp:
             cosine_sim = json.load(fp)
         with open('./cosSim/data.json', 'r') as fp:
             data = json.load(fp)
+
     # doc id가 cossinSim 리스트에 몇번째 것인지 파악해야 한다.
     ids = data["id"]
-    # rcndList = []
-        # print(i)
-    rcmdListAll = []
 
+    rcmdListAll = []
     for id in idList:
         try:
             index = ids.index(id)
@@ -84,14 +104,12 @@ def getRcmd(idList, calc = False):
                 # docIdx = oneRcmd[0]
                 # rcmdList.append()
 
-            # ids = data["id"]
             rcmdList = []
             for oneDoc in topFiveRcmd:
                 docIdx = oneDoc[0]#몇번째 문서인지 알려준다.
                 # 그 몇번째 문서가... id가 뭔지 찾아야 한다.
                 # rcmdList.append(ids[docIdx])#id을 담기
                 rcmdList.append(data["titles"][docIdx])#제목을 담기
-                # print(data["titles"][docIdx])
 
             rcmdListAll.append({"id" : id, "rcmd" : rcmdList})
 
@@ -100,24 +118,7 @@ def getRcmd(idList, calc = False):
     
 
     return rcmdListAll
-    # return { "id" : id, "rcmd" : rcndList }
-    """
-        angular
-            검색 결과 페이지에서 전체 id을 array으로 가지고 있다.
-            array id를 플라스크로 보낸다.
-        flask
-            array id을 받으면
-                각 id마다 추천 문서 5개를 뽑는다.
-                추천 문서 5개를 제목이 아니라... id으로 보내준다?
-                앵귤러가 가지고 있는 것은 ... 그냥 해당 id뿐이다...
-                앵귤러에 띄워야 할 정보들 : 추천 문서 제목, 주소.
-                추천 문서의 제목과 주소는 플라스크에서 만들어서 보내줘야 한다.
-                일단 제목만 받아서 보내는 것을 구현해보자.
-                그렇다면 받은 id에 
-                { "id" : "abcde", "rcmd" : [ "A", "B", "C", "D", "E" ] , "address" : [ "A", "B", "C", "D", "E" ]}
-                
-                디버깅을 위해서 띄워야 할 정보 : 그 문서의 내용?
-    """
+
 
 
 if __name__ == "__main__":
@@ -126,5 +127,3 @@ if __name__ == "__main__":
     data = prs.loadData(700)
     cosine_sim = getSimTbl(data["contents"])
 
-
-# print(getRcmd("5de1109d582a23c9693cbec9"))
