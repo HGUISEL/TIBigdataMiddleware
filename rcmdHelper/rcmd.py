158a07da0c5fb179a1032ee3a1408baf8b4c8ecc
BaekTree <etknbe@gmail.com>
2020-01-28 22:49:03 +0900

    rcmd module and package

--- rcmdHelper/rcmd.py
+++ rcmdHelper/rcmd.py
@@ -1,20 +1,28 @@
 ## phase 0. 디렉토리 추가
-## 현재 위치 : .\TIBigdataMiddleware\Labs\cosSim>...
+## 현재 위치 : .\TIBigdataMiddleware\cosSim\
+
+## 다른 모듈을 사용하기 위해  TIBigdataMiddleware 추가
 ## middleware home에도 dir 추가
-# from pathlib import Path
-# import os
-# curDir = os.getcwd()
-# curDir = Path(curDir)
-# homeDir = curDir.parent.parent
+from pathlib import Path
+import os
+curDir = os.getcwd()
+curDir = Path(curDir)
+homeDir = curDir.parent.parent
 # # comDir = homeDir / "common"
 
-# import sys
-# sys.path.append(str(homeDir))
+import sys
+sys.path.append(str(homeDir))
+
+## 다른 모듈에 사용 받기 위해 현재 이 디렉토리 추가
+file_dir = os.path.dirname(__file__)
+sys.path.append(file_dir)
 
 
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import linear_kernel
-import operator
+# static directory
+TFIDF_DIR = "./rcmdHelper/skl_tfidf.json"
+DATA_DIR = "./rcmdHelper/data.json"
+
+
 """
 # fnction : getSimTbl(data)
 # purpose : 전달받은 BoW에 대한 코사인 유사도 테이블을 만든다. sk-learn TF-IDF 사용.
@@ -28,6 +36,8 @@ import operator
 """
 def getSimTbl(data):
 
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from sklearn.metrics.pairwise import linear_kernel
     tfidf = TfidfVectorizer()
     tfidf_mtx = tfidf.fit_transform(data)
 
@@ -35,7 +45,7 @@ def getSimTbl(data):
 
     # save tfidf result
     import json
-    with open('./cosSim/skl_tfidf.json', 'w') as fp:
+    with open(TFIDF_DIR, 'w') as fp:
         json.dump(cosine_sim.tolist(), fp)
 
 
@@ -59,24 +69,22 @@ def getSimTbl(data):
     * calc = false으로 호출하면 기존에 저장해둔 정보를 사용한다. 아주 빠르게 프론트엔드에 응답해준다. defualt값.
 """
 def getRcmd(idList, calc = False):
-
-
     # phase 1: TF-IDF을 새로 업데이트하여 출력할 것인지 결정
     if calc == True:
     # phase 1-1: 문서 로드 및 전처리
         from common import prs
         data = prs.loadData(700)
         import json
-        with open('./cosSim/data.json', 'w') as fp:
+        with open(DATA_DIR, 'w') as fp:
             json.dump(data, fp)
 
         cosine_sim = getSimTbl(data["contents"])
     else:
     # phase 1-2: 저장되어 있는 tf-idf 값과 data 정보 불러옴
         import json
-        with open('./cosSim/skl_tfidf.json', 'r') as fp:
+        with open(TFIDF_DIR, 'r') as fp:
             cosine_sim = json.load(fp)
-        with open('./cosSim/data.json', 'r') as fp:
+        with open(DATA_DIR, 'r') as fp:
             data = json.load(fp)
 
     # doc id가 cossinSim 리스트에 몇번째 것인지 파악해야 한다.
@@ -90,7 +98,8 @@ def getRcmd(idList, calc = False):
 
             #recommendation table
             rcmdTbl = list(enumerate(cosine_sim[index]))
-
+            
+            import operator
             tempSort = sorted(rcmdTbl, key=operator.itemgetter(1), reverse=True)
             topFiveRcmd = []
             for i, oneRcmd in enumerate(tempSort):
@@ -121,9 +130,10 @@ def getRcmd(idList, calc = False):
 
 
 
-if __name__ == "__main__":
-    from common import prs
+# if __name__ == "__main__":
+#     from common import prs
 
-    data = prs.loadData(700)
-    cosine_sim = getSimTbl(data["contents"])
+#     data = prs.loadData(700)
+#     cosine_sim = getSimTbl(data["contents"])
+
 
