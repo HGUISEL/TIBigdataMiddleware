fef058f38728bebd0f9612cf53135218df9da81c
BaekTree <etknbe@gmail.com>
2020-02-19 16:33:29 +0900

    rcmd speed up

--- rcmdHelper/rcmd.py
+++ rcmdHelper/rcmd.py
@@ -45,8 +45,8 @@ def getSimTbl(data):
 
     # save tfidf result
     import json
-    with open(TFIDF_DIR, 'w') as fp:
-        json.dump(cosine_sim.tolist(), fp)
+    with open(TFIDF_DIR, 'w',encoding="utf-8") as fp:
+        json.dump(cosine_sim.tolist(), fp,ensure_ascii=False)
 
 
     return cosine_sim
@@ -71,56 +71,87 @@ def getSimTbl(data):
 def getRcmd(idList, calc = False):
     # phase 1: TF-IDF을 새로 업데이트하여 출력할 것인지 결정
     if calc == True:
-    # phase 1-1: 문서 로드 및 전처리
+    # phase 1-1: 문서 로드 및 새로 tfidf 테이블을 만든다.
         from common import prs
         data = prs.loadData(700)
         import json
-        with open(DATA_DIR, 'w') as fp:
-            json.dump(data, fp)
+        with open(DATA_DIR, 'w',encoding="utf-8") as fp:
+            json.dump(data, fp,ensure_ascii=False)
 
         cosine_sim = getSimTbl(data["contents"])
+        """
+            각 tfidf 테이블을 정렬하는 작업
+            현재 상태는 문서의 순서대로 각 문서의 연관 문서가 list에 들어가 있다.
+            연관문서를 표현하려면 연관순위가 높은 문서들을 표시해야 하므로,
+            문서 유사도가 높은 문서들을 앞으로 정렬한다.
+        """
+        import operator
+        
+        sort_cos_sim = []
+                                                 # for loop에서 type 변환해서 저장하는게 안되는 모양?
+        for i,oneDocRec in enumerate(cosine_sim):#for loop안에서 list element 새로 assgin => index으로 접근. 
+                                                 #for i, oneDocRec에서 oneDocRec은 local var이다.
+            oneDocRec = list(enumerate(oneDocRec))
+            sort_cos_sim.append(sorted(oneDocRec, key=lambda x: x[1], reverse=True))
+
+        # using map
+        # sort_cos_sim = map(lambda x : sorted(list(enumerate(x)), key=operator.itemgetter(1), reverse=True),cosine_sim)
+        # print(sort_cos_sim[0])
+
+        with open(TFIDF_DIR, 'w') as fp:
+            json.dump(sort_cos_sim, fp)
+
     else:
     # phase 1-2: 저장되어 있는 tf-idf 값과 data 정보 불러옴
         import json
         with open(TFIDF_DIR, 'r') as fp:
             cosine_sim = json.load(fp)
-        with open(DATA_DIR, 'r') as fp:
+        with open(DATA_DIR, 'r',encoding="utf-8") as fp:
             data = json.load(fp)
 
-    # doc id가 cossinSim 리스트에 몇번째 것인지 파악해야 한다.
+    # FE에서 요청한 각 문서가 cossinSim 리스트의 몇번째 문서인지 파악해야 한다.
     ids = data["id"]
 
     rcmdListAll = []
     for id in idList:
         try:
-            index = ids.index(id)
+            index = ids.index(id)# FE에서 요청한 각 문서의 index을 파악
         
 
             #recommendation table
             rcmdTbl = list(enumerate(cosine_sim[index]))
             
-            import operator
-            tempSort = sorted(rcmdTbl, key=operator.itemgetter(1), reverse=True)
-            topFiveRcmd = []
-            for i, oneRcmd in enumerate(tempSort):
+            # print(rcmdTbl)
+
+            # import operator
+            # tempSort = sorted(rcmdTbl, key=operator.itemgetter(1), reverse=True)
+            # topFiveRcmd = []
+            rcmdList = []
+            rcmdListId = []
+            for i, oneRcmd in enumerate(rcmdTbl):
                 # if i == 0:
                     # continue
                 if i > 5:
                     break
                 # print(str(i) + "th index fin!")
-                
-                topFiveRcmd.append(oneRcmd)
-                # docIdx = oneRcmd[0]
-                # rcmdList.append()
-
-            rcmdList = []
-            rcmdListId = []
-            for oneDoc in topFiveRcmd:
+                # topFiveRcmd.append(oneRcmd)
+                # for oneDoc in oneRcmd:
                 docIdx = oneDoc[0]#몇번째 문서인지 알려준다.
                 # 그 몇번째 문서가... id가 뭔지 찾아야 한다.
                 # rcmdList.append(ids[docIdx])#id을 담기
                 rcmdList.append(data["titles"][docIdx])#제목을 담기
                 rcmdListId.append(data["id"][docIdx])
+                # docIdx = oneRcmd[0]
+                # rcmdList.append()
+
+            # rcmdList = []
+            # rcmdListId = []
+            # for oneDoc in topFiveRcmd:
+            #     docIdx = oneDoc[0]#몇번째 문서인지 알려준다.
+            #     # 그 몇번째 문서가... id가 뭔지 찾아야 한다.
+            #     # rcmdList.append(ids[docIdx])#id을 담기
+            #     rcmdList.append(data["titles"][docIdx])#제목을 담기
+            #     rcmdListId.append(data["id"][docIdx])
 
             rcmdListAll.append({"id" : rcmdListId, "rcmd" : rcmdList})
             # rcmdListAll.append({"id" : id, "rcmd" : rcmdList})
