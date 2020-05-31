58bc13f985e7f6c668460d04519d29453afbb91a
BaekTree <etknbe@gmail.com>
2020-06-01 06:23:19 +0900

    file location

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 #-*- coding:utf-8 -*-
+import os
+import sys
+
+file_dir = os.path.dirname(__file__)
+sys.path.append(file_dir)
+
 from flask import Flask, jsonify, request, Response
 from flask_restful import Resource, Api
 from elasticsearch import Elasticsearch
