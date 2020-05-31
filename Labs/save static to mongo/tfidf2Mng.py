6f244dbda54453e1e9c522dc6d2944c4e97f045d
BaekTree <etknbe@gmail.com>
2020-05-31 22:51:08 +0900

    lab update

--- Labs/save static to mongo/tfidf2Mng.py
+++ Labs/save static to mongo/tfidf2Mng.py
@@ -7,6 +7,6 @@ with open(TFIDF_DATA_DIR,'r', encoding="utf-8") as fp:
 import pymongo
 from pymongo import MongoClient
 client = MongoClient('localhost',27017)
-db = client.analysis
+db = client.user
 collection = db.tfidf
 collection.insert_many(tfidfData)
