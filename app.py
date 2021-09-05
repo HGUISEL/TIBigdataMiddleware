475abe8dd0b4623987489a69c983f03e7a9317f5
BornToBeLoved <sungwon1685@gmail.com>
2021-09-06 00:54:23 +0900

    clusterNum error fix

--- app.py
+++ app.py
@@ -214,7 +214,7 @@ def textmining():
     
     # for kmeans
     elif analysisName == 'kmeans':
-        clusterNum = 3 # data["clusterNum"]
+        clusterNum = data["clusterNum"]
         print("kmeans 분석을 시작합니다\n")
         result = kmeans(email, keyword, savedDate, optionList, analysisName, clusterNum)
         print("\n kmeans 분석 결과\n")
