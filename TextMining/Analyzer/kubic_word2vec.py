da5f847a7960611deac86227c70fe4bb5524821d
BornToBeLoved <sungwon1685@gmail.com>
2022-01-24 19:35:43 +0900

    error fix(deleted test code)

--- TextMining/Analyzer/kubic_word2vec.py
+++ TextMining/Analyzer/kubic_word2vec.py
@@ -21,6 +21,22 @@ import traceback
 
 logger = logging.getLogger("flask.app.word2vec")
 
+# 리스트 받아서 0~1값으로 min-maz normalize
+def normalization(x):
+    min_value = min(x)
+    max_value = max(x) 
+
+    return list(map(lambda x: (x-min_value)/(max_value-min_value), x))
+
+def make_normalization_model(un_norm_list):
+    norm_list = normalization(un_norm_list)
+    model_dict = dict()
+    for i in range(len(un_norm_list)):
+        model_dict[un_norm_list[i]] = norm_list[i]
+    return model_dict
+
+
+
 def cut_with_option(list,email, keyword, savedDate, optionList):
     top_words = word_count(email, keyword, savedDate, optionList, "wordcount")[0]
     result_lst = []
@@ -29,13 +45,14 @@ def cut_with_option(list,email, keyword, savedDate, optionList):
             result_lst.append(word)
         else:
             logger.info(identification + "단어가 모델에 존재하지 않습니다. 단어:" + word)
-    print(top_words)
-    return result_lst
+    #print(top_words)
+
+    return result_lst, top_words
 
 def word2vec(email, keyword, savedDate, optionList, analysisName):
     identification = str(email)+'_'+analysisName+'_'+str(savedDate)+"// "
 
-    try: # 수정해야함
+    try: # 오류메세지 수정해야함
         int(optionList)
         if not(0 <= int(optionList)):
             raise Exception("군집의 수는 양의 정수여야 합니다 입력된 값:" + str(optionList))
@@ -64,7 +81,7 @@ def word2vec(email, keyword, savedDate, optionList, analysisName):
 
         w2v = Word2Vec(preprocessed, min_count =1)
         #print(w2v)
-        print(w2v.wv.most_similar("최고"))
+        # print(w2v.wv.most_similar("최고"))
 
     except Exception as e:
         err = traceback.format_exc()
@@ -76,7 +93,7 @@ def word2vec(email, keyword, savedDate, optionList, analysisName):
         
         w2v_tsne = TSNE(n_components=2)
         #w2v_vocab = list(w2v.wv.key_to_index)
-        w2v_vocab = cut_with_option(list(w2v.wv.key_to_index), email, keyword, savedDate, optionList)
+        w2v_vocab, top_words = cut_with_option(list(w2v.wv.key_to_index), email, keyword, savedDate, optionList)
         # w2v_vocab = list(w2v.wv.key_to_index.keys())
         #print(w2v_vocab[0:10]) 
         w2v_similarity = w2v.wv[w2v_vocab] # 기존: w2v[word] --> 바뀐 코드: w2v.wv[word]
@@ -85,7 +102,7 @@ def word2vec(email, keyword, savedDate, optionList, analysisName):
 
         # print(w2v_df[0:10])
 
-        w2v_df = w2v_df[0:10]
+        w2v_df = w2v_df[0:int(optionList)]
 
         indexList = [ item for item in w2v_df.index]
         # https://observablehq.com/@d3/scatterplot-with-shapes
@@ -93,15 +110,18 @@ def word2vec(email, keyword, savedDate, optionList, analysisName):
         jsonDict = dict()
         textTSNEList = list()
         
+        # 시각화를 위한 빈도수 정규화 모델
+        norm_dict = make_normalization_model(list(top_words.values()))
         for i in range(len(indexList)):
             word = indexList[i]
             textDict = dict()
             textDict["word"] = word
-            textDict["x"] = int(w2v_df["x"][word])
-            textDict['y'] = int(w2v_df["y"][word])
+            textDict["x"] = float(w2v_df["x"][word])
+            textDict['y'] = float(w2v_df["y"][word])
+            textDict['wcount'] = norm_dict[top_words[word]]
             textTSNEList.append(textDict)
 
-        print(textTSNEList)
+        #print(textTSNEList)
     except Exception as e:
         err = traceback.format_exc()
         logger.error(identification+"word2vec 모델 만들기에 실패하였습니다. \n"+str(err))
@@ -109,4 +129,4 @@ def word2vec(email, keyword, savedDate, optionList, analysisName):
 
     return True, textTSNEList
 
-#word2vec('21800520@handong.edu', '북한', "2021-08-10T10:59:29.974Z", "3", 'word2vec')
+#word2vec('21800520@handong.edu', '북한', "2021-08-10T10:59:29.974Z", "20", 'word2vec')
