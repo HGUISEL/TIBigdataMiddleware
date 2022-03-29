f6cafc68819132634edbc6740784869bffe59430
BornToBeLoved <sungwon1685@gmail.com>
2022-03-29 11:54:51 +0000

    [FIX] fixed by someone

--- relatedDoc_all.py
+++ relatedDoc_all.py
@@ -11,6 +11,9 @@ from common import cmm
 sys.path.append(os.path.abspath('/home/middleware/TIBigdataMiddleware/topic_analysis'))
 import esAccount as esAcc
 
+import warnings
+warnings.filterwarnings("ignore")
+
 es = Elasticsearch(
     [esAcc.host],
     http_auth=(esAcc.id, esAcc.password),
@@ -32,24 +35,25 @@ def create_similiarity(data = None):
 def makeCorpus (resp):
     corpus = []
     for oneDoc in resp['hits']['hits']:
-            print(oneDoc["_source"]["post_title"])    
-            print(oneDoc["_source"]["hash_key"])
-            if "file_extracted_content" in oneDoc["_source"].keys():
-                corpus.append(
-                    {
-                        "hashkey" : oneDoc["_source"]["hash_key"],
-                        "post_title" : oneDoc["_source"]["post_title"],
-                        "content" : oneDoc["_source"]["file_extracted_content"]
-                    }
-                )
-            else:
-                corpus.append(
-                    {
-                        "hashkey" : oneDoc["_source"]["hash_key"],
-                        "post_title" : oneDoc["_source"]["post_title"],
-                        "content" : oneDoc["_source"]["post_body"]
-                    }
-                 )
+            try: 
+                if "file_extracted_content" in oneDoc["_source"].keys():
+                    corpus.append(
+                        {
+                            "hashkey" : oneDoc["_source"]["hash_key"],
+                            "post_title" : oneDoc["_source"]["post_title"],
+                            "content" : oneDoc["_source"]["file_extracted_content"]
+                        }
+                    )
+                else:
+                    corpus.append(
+                        {
+                            "hashkey" : oneDoc["_source"]["hash_key"],
+                            "post_title" : oneDoc["_source"]["post_title"],
+                            "content" : oneDoc["_source"]["post_body"]
+                        }
+                    )
+            except KeyError as e:
+                continue
     return corpus
 
 def filterEmptyDoc (corpus):
@@ -68,7 +72,6 @@ def filterEmptyDoc (corpus):
 
 def processData(resp):
     # Create corpus object
-    print("Create corpus")
     corpus = makeCorpus(resp)
 
     return corpus
@@ -78,7 +81,6 @@ def createJson(dir, result, count):
     with open("{dir}{num}.json".format(dir=dir, num=count), 'w', -1, "utf-8") as f:
         json.dump(result, f, ensure_ascii=False)
 
-print("start")
 analysisResult = []
 count = 0
 # Get first 100 data
@@ -88,16 +90,19 @@ resp = es.search(
     body = { "size":100, "query": { "match_all" : {} } },    
     scroll='10m'
 )
-print("get first 100 data")
+#print("get first 100 data")
 c = processData(resp)
 createJson(DATA_DIR, filterEmptyDoc(c), count)
-analysisResult = analysisResult + c
-print("analysis data is collected")
+###original data
+#analysisResult = analysisResult + c
+
+###twice data
+analysisResult = analysisResult + c + c
 
 scrollId = resp["_scroll_id"]
 while len(resp['hits']['hits']):
     count = count + 1
-    print("start set #{}".format(count))
+    #print("start set #{}".format(count))
     resp = es.scroll( 
         scroll_id = scrollId, 
         scroll='10m'
@@ -105,20 +110,19 @@ while len(resp['hits']['hits']):
 
     c = processData(resp)
     createJson(DATA_DIR, filterEmptyDoc(c), count)
-    analysisResult = analysisResult + c
+    ###original data
+    #analysisResult = analysisResult + c
+    ###twice data
+    analysisResult = analysisResult + c + c
 
 analysisResult = filterEmptyDoc(analysisResult)
 
-print("all data is collected and empty docs are filtered")
-print("test shut down")
-sys.exit(0)
 # global cosine_sim
 global cosine_sim
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
-print("start vectorize")
 tfidf = TfidfVectorizer()
 for i, d in reversed(list(enumerate(analysisResult['contents']))):
     if d == None:
@@ -126,26 +130,27 @@ for i, d in reversed(list(enumerate(analysisResult['contents']))):
         analysisResult['id'].pop(i)
         analysisResult['titles'].pop(i)
 
-
-print("transform")
+print("number of documents: ", len(analysisResult['contents']))
+print("Computing TF-IDF Values...")
 tfidf_mtx = tfidf.fit_transform(analysisResult['contents'])
-
-print("lenear kernel")
+print("Computing cosine similarity...")
+s_time = time.time()
 cosine_sim = linear_kernel(tfidf_mtx, tfidf_mtx)
+print("It took ",time.time()-s_time," seconds")
 # createJson(TFIDF_DIR, cosine_sim.tolist(), count)
 
 import operator
     
 sort_cos_sim = []
-print("create table")
+#print("create table")
 for i,oneDocRec in enumerate(cosine_sim):
     oneDocRec = list(enumerate(oneDocRec))
     sort_cos_sim.append(sorted(oneDocRec, key=lambda x: x[1], reverse=True)[:20])
     createJson(TABLE_DIR, sorted(oneDocRec, key=lambda x: x[1], reverse=True)[:20], i)
 
-print("get result and create json")
-print(len(sort_cos_sim))
-print(len(analysisResult['id']))
+#print("get result and create json")
+#print(len(sort_cos_sim))
+#print(len(analysisResult['id']))
 
 for index, id in enumerate(analysisResult['id']):
     rcmdTbl = sort_cos_sim[index]
@@ -158,5 +163,5 @@ for index, id in enumerate(analysisResult['id']):
         oneDocRcmd = {"hashkey" : id, "rcmd" : rcmdList}
     createJson(RESULT_DIR, [oneDocRcmd], index)
 
-print("Done")
+#print("Done")
 cmm.showTime()
